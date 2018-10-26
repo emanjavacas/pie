1a85c83aa42de234cbe1e3ffdb38c235bf18e5a3
Thibault Clérice <leponteineptique@gmail.com>
2018-10-26 15:07:58 +0200

    Proposal for a simple webapp included in Pie

---
+++ app.py
@@ -0,0 +1,115 @@
+"""
+
+This module can be run by install the app requirements (`pip install -r requirements-app.txt`)
+
+The goal here is to be able to have, later, a distributed series of APIs that could be using different version of Pie
+and run through containers, unified by a public API such as https://github.com/hipster-philology/deucalion which would
+then talk to the different micro-services.
+
+How to run for development :
+    PIE_MODEL=/home/thibault/dev/pie/model-lemma-2018_10_23-14_05_19.tar FLASK_ENV=development flask run
+
+    where PIE_MODEL is the path to your model
+
+How to run in production :
+    gunicorn --workers 2 app:app --env PIE_MODEL=/home/thibault/dev/pie/model-lemma-2018_10_23-14_05_19.tar
+
+    Probably add to this a --bind
+
+Example URL:
+    http://localhost:5000/?data=Ci+gist+saint+Martins+el+sains+de+tours.%20Il%20fut%20bon%20rois.
+
+Example curl :
+    curl --data "data=Ci gist saint Martins el sains de tours. Il fut bon rois." http://localhost:5000
+
+Example output :
+    token	lemma	morph	pos
+    ci	ci	DEGRE=-	ADVgen
+    gist	jesir	MODE=ind|TEMPS=pst|PERS.=3|NOMB.=s	VERcjg
+    saint	saint	NOMB.=s|GENRE=f|CAS=r	ADJqua
+    martins	martin	NOMB.=s|GENRE=m|CAS=r	NOMcom
+    el	en1+le	NOMB.=s|GENRE=m|CAS=r	PRE.DETdef
+    sains	sain	NOMB.=p|GENRE=m|CAS=r	NOMcom
+    de	de	MORPH=empty	PRE
+    tours	tor2	NOMB.=p|GENRE=f|CAS=r	NOMcom
+    .	.	_	PONfrt
+    il	il	PERS.=3|NOMB.=s|GENRE=m|CAS=n	PROper
+    fut	estre1	MODE=ind|TEMPS=psp|PERS.=3|NOMB.=s	VERcjg
+    bon	bon	NOMB.=s|GENRE=m|CAS=n|DEGRE=p	ADJqua
+    rois	roi2	NOMB.=s|GENRE=m|CAS=n	NOMcom
+    .	.	_	PONfrt
+
+"""
+from flask import Flask, request, Response
+from pie.tagger import Tagger, simple_tokenizer
+from pie.utils import chunks
+import re
+from os import getenv
+
+model_file = getenv("PIE_MODEL")
+LOWER = getenv("PIE_LOWER", True)
+BATCH = int(getenv("PIE_BATCH", 32))
+DEVICE = getenv("PIE_DEVICE", "cpu")
+
+app = Flask(__name__)
+tagger = Tagger(device=DEVICE, batch_size=BATCH, lower=LOWER)
+
+
+def model_spec(inp):  # Taken from tag.py. Maybe it could be in utils to avoid such a repetition ?
+    """
+    >>> example = 'model-pos-2018:03:05.tar'
+    >>> model_spec(example)
+    [('model-pos-2018:03:05.tar', [])]
+
+    >>> example = '<model-pos-2018:03:05.tar,pos><model-pos-2018:03:05.tar,lemma>'
+    >>> model_spec(example)
+    [('model-pos-2018:03:05.tar', ['pos']), ('model-pos-2018:03:05.tar', ['lemma'])]
+    """
+    if not inp.startswith('<'):
+        return [(inp, [])]
+
+    output = []
+    for string in re.findall(r'<([^>]+)>', inp):
+        model_path, *tasks = string.split(',')
+        output.append((model_path, tasks))
+
+    return output
+
+
+for model, tasks in model_spec(model_file):
+    tagger.add_model(model, *tasks)
+    tasks = tasks or tagger.models[-1][0].label_encoder.tasks
+
+
+def iter_data(data, lower=LOWER):
+    for sentence in simple_tokenizer(data, lower=lower):
+        yield sentence, len(sentence)
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "GET":
+        data = request.args.get("data")
+    else:
+        data = request.form.get("data")
+
+    if not data:
+        return ""
+
+    output = ""
+    header = False
+    for chunk in chunks(iter_data(data), size=BATCH):
+        sents, lengths = zip(*chunk)
+
+        tagged, tasks = tagger.tag(sents=sents, lengths=lengths)
+        sep = "\t"
+        for sent in tagged:
+            if not header:
+                output += sep.join(['token'] + tasks) + '\r\n'
+                header = True
+            for token, tags in sent:
+                output += sep.join([token] + list(tags)) + '\r\n'
+    return Response(
+        output,
+        mimetype="text/plain"
+    )
