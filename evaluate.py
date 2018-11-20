cd42ee75847a39e35f513c4506ee8be3b8cff746
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2018-11-20 13:08:34 +0100

    fix evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -10,12 +10,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('model_path')
     parser.add_argument('test_path', help="unix string", nargs='+')
+    parser.add_argument('train_path',
+                        help="needed to compute ambiguous and unknown tokens")
     parser.add_argument('--settings', help="settings file used for training")
     parser.add_argument('--batch_size', type=int, default=500)
     parser.add_argument('--buffer_size', type=int, default=100000)
     parser.add_argument('--device', default='cpu')
     parser.add_argument('--model_info', action='store_true')
     parser.add_argument('--full', action='store_true')
+    parser.add_argument('--use_beam', action='store_true')
+    parser.add_argument('--beam_width', default=10, type=int)
     args = parser.parse_args()
 
     model = BaseModel.load(args.model_path).to(args.device)
@@ -36,9 +40,12 @@ if __name__ == '__main__':
     settings.buffer_size = args.buffer_size
     settings.device = args.device
 
-    reader = Reader(settings, *args.test_path)
-    dataset = Dataset(settings, reader, model.label_encoder)
-    dataset = device_wrapper(list(dataset.batch_generator()), args.device)
+    trainset = None
+    if args.train_path:
+        trainset = Dataset(
+            settings, Reader(settings, args.train_path), model.label_encoder)
 
-    for task in model.evaluate(dataset).values():
+    testset = Dataset(settings, Reader(settings, *args.test_path), model.label_encoder)
+
+    for task in model.evaluate(testset, trainset).values():
         task.print_summary(full=args.full)
