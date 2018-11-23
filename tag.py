68ffbee79c12ef0b5899debbd49995e6cdbce3f2
Thibault Clérice <1929830+PonteIneptique@users.noreply.github.com>
2018-11-23 17:33:55 +0100

    Fix tag with latests change to utils.py

--- tag.py
+++ tag.py
@@ -1,6 +1,3 @@
-
-import os
-
 from pie import utils
 from pie.tagger import Tagger
 
@@ -8,7 +5,7 @@ from pie.tagger import Tagger
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('model_spec', type=model_spec)
+    parser.add_argument('model_spec', type=utils.model_spec)
     parser.add_argument('input_path', help="unix string")
     parser.add_argument('--batch_size', type=int, default=50)
     parser.add_argument('--device', default='cpu')
@@ -16,13 +13,13 @@ if __name__ == '__main__':
     parser.add_argument('--beam_width', default=10, type=int)
     parser.add_argument('--lower', action='store_true')
     args = parser.parse_args()
-
+    
     tagger = Tagger(device=args.device, batch_size=args.batch_size, lower=args.lower)
 
-    for model, tasks in utils.model_spec(args.model_spec):
+    for model, tasks in args.model_spec:
         tagger.add_model(model, *tasks)
         print(" - model: {}".format(model))
-        tasks = tasks or list(model.label_encoder.tasks)
+        tasks = tasks or tagger.models[-1][0].label_encoder.tasks
         print(" - tasks: {}".format(", ".join(tasks)))
 
     for fpath in utils.get_filenames(args.input_path):
